7cc0cfc1cc36d1d3ce324909892d2ce2cc2e28ae
Sohaib Segueni <sohaibsegueni8@gmail.com>
2022-09-20 22:07:17 +0100

    checkout page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,7 @@ import { useRef } from "react";
 import useDraggableScroll from "use-draggable-scroll";
 import HeroForm from "../../Components/Forms/HeroForm";
 import { Footer, NavbarTop } from "../../Components/Navigation/Navigation";
+import { GridDisplay, ScrollDisplay } from "../CarDisplayViews";
 
 function HeroSection() {
   return (
@@ -63,35 +64,8 @@ export default function Home() {
       <div className="page py-5 px-4 md:py-8 md:px-16">
         <HeroSection></HeroSection>
 
-        <section className="popular flex flex-col gap-4">
-          <div className="flex flex-row justify-between">
-            <p className="text-light">Popular Cars</p>
-            <a href="#" className="fw-semibold">
-              View All
-            </a>
-          </div>
-          <div
-            onMouseDown={onMouseDown}
-            ref={ref}
-            className="popular__container flex flex-row gap-6 overflow-scroll"
-          >
-            {getCarArray(4).map((car, index) => (
-              <CatalogueCard car={car} key={index} />
-            ))}
-          </div>
-        </section>
-        <section className="recommended flex flex-col gap-4 mt-6">
-          <p className="text-light">Recommended</p>
-          <div className=" grid grid-cols-[repeat(auto-fit,_minmax(18rem,_1fr))] gap-6">
-            {getCarArray(10).map((car, index) => (
-              <CatalogueCard car={car} key={index} />
-            ))}
-          </div>
-          <BuAccent
-            text="View More"
-            className="py-4 px-6  rounded-sm m-auto mt-4"
-          />
-        </section>
+        <ScrollDisplay/>
+        <GridDisplay/>
       </div>
       <Footer></Footer>
     </>
